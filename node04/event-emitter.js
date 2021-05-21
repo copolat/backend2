1f8bcab55bcc1c74af294789bad8e0ddbbeb696d
copolat <copolat@yahoo.com>
2021-05-21 11:38:20 -0500

    File IO -read, write, delete-, HTTP Server

--- node04/event-emitter.js
+++ node04/event-emitter.js
@@ -1,10 +1,17 @@
 const events = require('events')
 
 const eventEmitter = new events.EventEmitter()
-
+// ()=>{}
+/*
+eventEmitter.on('sayName',()=>{
+    console.log('My name is John')
+})
+*/
 eventEmitter.on('sayName',(text)=>{
     console.log('My name is ', text.name, 'I am ', text.age, ' years old')
 })
 
+
+// eventEmitter.emit('sayName')
 eventEmitter.emit('sayName', {name:"John", age:42})
 
---
+++ node05/file-read.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+fs.readFile('demo.txt', (error, data)=>{
+  if(error){
+    console.log("An error occured during reading the file: ", error)
+  }
+  console.log(data.toString())
+})
+
+
+console.log("File read ends....")
