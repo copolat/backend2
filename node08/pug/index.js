d6909ec93ae5b12be3d4832c07aae15aa13b988e
copolat <copolat@yahoo.com>
2021-05-27 22:10:53 -0500

    PUG -Variables, Conditionals, Iterations, Templetes-, Static Folders, Routing with ExpressJS Intro

--- node08/pug/index.js
+++ node08/pug/index.js
@@ -7,5 +7,7 @@ app.set('view engine', 'pug')
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.render('index.pug', {name:'Demetrius', phone:'(803) 206-1988 x873'}))
+app.get('/home', (req, res)=>{res.render('home.pug')})
+app.get('/contact', (request, response)=>response.render('contact'))
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ node08/static-folder/index.js
@@ -0,0 +1,12 @@
+// node-express snippet
+const express = require('express')
+const app = express()
+const port = 3000
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+app.use(express.static('public2'))
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index'))
+app.get('/index2', (req, res) => res.render('index2'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
