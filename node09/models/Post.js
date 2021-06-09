7835cef67ea0e13e2a88660d66a6c4dde7ab3f96
copolat <copolat@yahoo.com>
2021-06-08 22:25:06 -0500

    Mongoose queries -Find(), FindOne(), FindById(), UpdatMany()-

--- node09/models/Post.js
+++ node09/models/Post.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema
 
 const PostSchema = new Schema({
   title: String,
-  createdDate: Date
+  subTitle: {type: String, required:true, unique:true},
+  createdDate: {type:Date, default:Date.now},
+  isActive: {type:Boolean, default:true},
+  comments: [{message:String}],
+  meta: {votes: Number, favs: Number}
 })
 
 module.exports = mongoose.model('post', PostSchema) 
--- node09/routes/post.routes.js
+++ node09/routes/post.routes.js
@@ -5,14 +5,94 @@ const PostModel = require('../models/Post')
 // Created by node-express-post snippet
 router.post('/', function (req, res) {
   const myPostModel = new PostModel({
-    title: "My New Ttitle 2",
-    createdDate: Date.now()
+    title: "My New Title 11",
+    subTitle: "My new subtitle 2",
+    //createdDate: Date.now(),
+    isActive: false,
+    meta:{votes:54, favs: 45},
+    comments: [
+      {message:"Good job!!!"},
+      {message:"It's fair..."}
+    ]
+
   })
   myPostModel.save((error, data)=>{
     if(error) res.json(error)
     res.json(data)
   })
-  //res.send('POST request to the homepage')
+})
+
+// Find() --> To get all data from the collection
+/*
+router.get('/', (req, res) => {
+  PostModel.find((resultData,err)=>{
+    if(err) res.json(err)
+    res.json(resultData)})
+})
+*/
+
+router.get('/', (req, res) => {
+  PostModel.find()
+  .then((resultData)=>{res.json(resultData)})
+  .catch((err)=>{res.json(err)})
+})
+
+// Filter the results in find()
+/*
+router.get('/find', (req, res) => {
+  PostModel.find({isActive:false, title:"My New Title 6"})
+  .then((resultData)=>{res.json(resultData)})
+  .catch((err)=>{res.json(err)})
+})
+*/
+router.get('/find', (req, res) => {
+  PostModel.find({isActive:false, title:"My New Title 10"}, 'title subTitle')
+  .then((resultData)=>{res.json(resultData)})
+  .catch((err)=>{res.json(err)})
+})
+// FindOne()---> Get the first data that matches with the conditions
+router.get('/findOne', (req, res) => {
+  PostModel.findOne({isActive:false, title:"My New Title 10"})
+  .then((resultData)=>{res.json(resultData)})
+  .catch((err)=>{res.json(err)})
+})
+// FindById()---> According to an ID, it will return that result
+router.get('/findById', (req, res) => {
+  PostModel.findById('60c00cc7765db72da44eba4b')
+  .then((resultData)=>{res.json(resultData)})
+  .catch((err)=>{res.json(err)})
+})
+
+// Get the data from user request
+router.post('/add', function (req, res) {
+  const myPostModel = new PostModel({
+    title: req.body.title,
+    subTitle: req.body.subTitle,
+    //createdDate: Date.now(),
+    isActive: req.body.isActive,
+    meta:req.body.meta,
+    comments: req.body.comments
+
+  })
+  myPostModel.save((error, data)=>{
+    if(error) res.json(error)
+    res.json(data)
+  })
+})
+// Get the data from user request 2
+router.post('/addAll', function (req, res) {
+  const myPostModel = new PostModel(req.body)
+  myPostModel.save((error, data)=>{
+    if(error) res.json(error)
+    res.json(data)
+  })
+})
+
+// Update()
+router.put('/updateMany',(req, res)=>{
+  PostModel.updateMany({isActive:false},{title:'Title has been updated....'})
+  .then((data)=>{res.json(data)})
+  .catch((error)=>{res.json(error)})
 })
 
 module.exports = router
